68268c0b32b506302bf5c6c78b058b75ae0a4fa9
Vamsi <bvamsi489@gmail.com>
2024-03-02 23:40:46 +0530

    form working. form data not showing on refresh

--- customform/src/pages/Form.jsx
+++ customform/src/pages/Form.jsx
@@ -1,13 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './form.css'
 
 const Form = () => {
+
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const handleSubmit = () => {
+    console.log("email")
+  }
   return (
     <div className='form-container'>
       <h1>Form</h1>
-      <label htmlFor="">Name</label>
-      <input type="text" />
-      <br />
+      <form onSubmit={handleSubmit}>
+        <label>Email</label>
+        <input
+         type='email'
+          />
+        <br />
+        <label>Password</label>
+        <input
+         type='password'
+          />
+        <br />
+        <button>Submit</button>
+      </form>
+
     </div>
   )
 }
